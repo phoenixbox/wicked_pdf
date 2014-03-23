1d39e5e29cad62eb12257013465ee823b1265abe
phoenixbox <shane@quickleft.com>
2014-03-22 21:03:07 -0600

    added new binaries

--- Gemfile
+++ Gemfile
@@ -2,7 +2,7 @@ source 'https://rubygems.org'
 
 gem 'rails', '4.0.3'
 
-gem 'wkhtmltopdf-binary'
+# gem 'wkhtmltopdf-binary'
 gem 'wicked_pdf'
 
 gem "raphael-rails", "~> 2.1.2"
--- config/initializers/wicked_pdf_config.rb
+++ config/initializers/wicked_pdf_config.rb
@@ -2,4 +2,6 @@ if Rails.env.production?
   # wkhtmltopdf_path = "#{Rails.root}/bin/wkhtmltopdf-linux-amd64"
   wkhtmltopdf_path = "#{Rails.root}/bin/wkhtmltox-linux-amd64"
   WickedPdf.config = { exe_path: wkhtmltopdf_path, wkhtmltopdf: wkhtmltopdf_path }
+else
+
 end
